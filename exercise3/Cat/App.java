7a061a8c658b9f7da45c799d3b0375d4c346dc12
lhara0904 <130621743+lhara0904@users.noreply.github.com>
2023-05-11 10:59:59 +0800

    Add files via upload

---
+++ exercise3/Cat/App.java
@@ -0,0 +1,13 @@
+package Cats;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        
+        Bursian myPet = new Pet();
+
+        myPet.bark();
+        System.out.println(myPet.eyecolor);
+        System.out.println(myPet.mouth);
+    }
+}
---
+++ exercise3/Cat/Pet.java
@@ -0,0 +1,9 @@
+package Cats;
+
+public class Pet implements Bursian {
+
+    
+    public void bark() {
+        System.out.println("My cute Siamaine is meowing");
+    };
+}
